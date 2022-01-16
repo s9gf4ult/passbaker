c0e363f6605481d57e7977350f7141a63d712c0e
Aleksey Uymanov <s9gf4ult@gmail.com>
2022-01-16 22:25:24 +0500

    pass done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-use clap::Parser;
+use pbkdf2 ::{
+    Pbkdf2,
+    password_hash::{
+        rand_core::OsRng,
+        SaltString, PasswordHasher, PasswordVerifier, PasswordHash
+    },
+};
+use std::io ;
+
 
 fn main() {
-    println!("Hello, world!");
+    let pwd = b"pass\n" ;
+    let salt = SaltString::generate(&mut OsRng);
+    let hash = Pbkdf2.hash_password(pwd, &salt).unwrap().to_string();
+    println!("{}", hash);
+    let parsed = PasswordHash::new(&hash).unwrap() ;
+    println!("{:?}", &parsed);
+    let mut pwd = String::new() ;
+    let mut stdin = io::stdin() ;
+    stdin.read_line(&mut pwd).unwrap() ;
+
+    let res = Pbkdf2.verify_password(pwd.as_bytes(), &parsed) ;
+
+    println!("{:?}", &res) ;
 }
