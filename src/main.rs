416f103ad0732b21e69b71f7e37fa115b27c300c
Aleksey Uymanov <s9gf4ult@gmail.com>
2022-02-10 00:26:52 +0500

    fix

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,21 @@ use pass_record::{
 use std::{
     path::Path
 } ;
-    // let pwd = b"pass" ;
-    // let salt = SaltString::generate(&mut OsRng);
-    // let hash = Pbkdf2.hash_password(pwd, &salt).unwrap().to_string();
-    // println!("{}", hash);
-    // let parsed = PasswordHash::new(&hash).unwrap() ;
-    // println!("{:?}", &parsed);
-    // let mut pwd = String::new() ;
-    // let stdin = io::stdin() ;
-    // stdin.read_line(&mut pwd).unwrap() ;
 
-    // let res = Pbkdf2.verify_password(pwd.as_bytes(), &parsed) ;
+use rpassword::read_password_from_tty ;
 
-    // println!("{:?}", &res) ;
+fn notifier(s: &str) {
+    print!("{}\n", s);
+}
+
 fn main() -> Result<(), PRError> {
     let args = Cli::parse() ;
     match args {
         Cli::New {name} => {
-            let mut pass = PassRecord::init(Path::new("~/.passbaker/"))? ;
+            let asker = || {
+                read_password_from_tty(Some("Password: ")).unwrap()
+            } ;
+            let mut pass = PassRecord::init(Path::new("/home/razor/.passbaker/"), &asker, &notifier, name)? ;
             pass.seedCyccle() ;
             Ok(())
         },
--- src/pass_record.rs
+++ src/pass_record.rs
@@ -3,50 +3,75 @@ use pbkdf2 ::{
     password_hash::{
         rand_core::OsRng,
         SaltString, PasswordHasher, PasswordVerifier, PasswordHash,
-        errors::Error
-
+        errors as password_hash_errors
     },
 };
 use std::io ;
 use std::path::{Path} ;
 use chrono::prelude::* ;
 use std::todo ;
+use std::fs::* ;
 
 pub struct PassRecord<'a> {
     hash: PasswordHash<'a>,
+    name: String,
     records: Vec<PassAttempt>
 }
 
 #[derive(Debug)]
 pub enum PRError {
-    PasswordHashError(Error)
+    PasswordHashError(password_hash_errors::Error),
+    IOError(io::Error),
+    HomeDirectoryError(String)
 }
 
-impl From<Error> for PRError {
-    fn from(pe: Error) -> PRError { PRError::PasswordHashError(pe) }
+impl From<password_hash_errors::Error> for PRError {
+    fn from(pe: password_hash_errors::Error) -> PRError { PRError::PasswordHashError(pe) }
+}
+
+impl From<io::Error> for PRError {
+    fn from(pe: io::Error) -> PRError { PRError::IOError(pe) }
 }
 
 impl <'a> PassRecord<'a> {
     // Initiates the new record by asking the password twice and creating all
     // files for further operation.
-    pub fn init(dir: &Path) -> Result<Self,PRError> {
-        let stdin = io::stdin() ;
-        let pass1 = {
-            let mut s = String::new() ;
-            stdin.read_line(&mut s) ;
-            s
-        } ;
+    pub fn init ( dir: &Path,
+                  asker: &(dyn Fn() -> String),
+                  notifier: &(dyn Fn(&str) -> ()),
+                  name: String
+    ) -> Result<PassRecord<'a> ,PRError> {
         let salt = SaltString::generate(&mut OsRng) ;
-        let hash = Pbkdf2.hash_password(pass1.as_bytes(), &salt)? ;
-        let pass2 = {
-            let mut s = String::new() ;
-            stdin.read_line(&mut s) ;
-            s
+        let hash = {
+            let pass = asker() ; // Ask user for password
+            Pbkdf2.hash_password(pass.as_bytes(), &salt.clone())?
         } ;
-        Pbkdf2.verify_password(pass2.as_bytes(), &hash)?;
-        // hash.verify_password(&pass2.as_bytes(), hash);
+        notifier("Repeat the password") ;
+        let pass2 = asker() ; // Repeat user password and recheck it
+        Pbkdf2.verify_password(pass2.as_bytes(), &hash)? ;
 
-        todo!()
+        match dir.metadata() {
+            Err(e) => match e.kind() {
+                io::ErrorKind::NotFound => {
+                    // Ok just create the directory
+                    create_dir(dir)?;
+                },
+                _ => return Err(PRError::from(e))
+            },
+            Ok(meta) => {
+                if  !meta.file_type().is_dir() {
+                    return Err(PRError::HomeDirectoryError("File name exists but this is not a directory".to_string()))
+                }
+            }
+        } ;
+
+        let result = PassRecord {
+            hash: hash,
+            name: name,
+            records: vec![]
+        } ;
+
+        Ok(result)
     }
     pub fn seedCyccle(&self) {
         todo!();
