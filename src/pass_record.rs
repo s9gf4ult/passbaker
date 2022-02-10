a8cf5e7cb01cea33f240adf0593ee5e38b28c1c3
Aleksey Uymanov <s9gf4ult@gmail.com>
2022-02-11 02:53:10 +0500

    serde

--- src/pass_record.rs
+++ src/pass_record.rs
@@ -1,10 +1,13 @@
+use core::fmt ;
 use pbkdf2::Pbkdf2 ;
 use std::io ;
 use std::path::{Path} ;
 use chrono::prelude::* ;
 use std::todo ;
 use std::fs::* ;
-use serde::* ;
+use serde:: {
+    *, de, de::Visitor
+} ;
 use password_hash:: {
     rand_core::OsRng,
     SaltString, PasswordHasher, PasswordVerifier, PasswordHash,
@@ -24,12 +27,36 @@ where
     ser.serialize_str(& p.to_string())
 }
 
+struct PHVisitor ;
+
+impl<'de> Visitor<'de> for PHVisitor {
+    type Value = PasswordHash<'de> ;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "a string containing password hash")
+    }
+
+    fn visit_borrowed_str<E>(self, s: &'de str) -> Result<PasswordHash<'de>, E>
+    where
+        E: de::Error,
+    {
+        PasswordHash::new(s).map_err(de::Error::custom)
+    }
+}
+
+fn password_hash_deserialize<'de, D>(de: D) -> Result<PasswordHash<'de>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    de.deserialize_str(PHVisitor)
+}
 
 #[derive(Serialize)]
 pub struct PassHeader<'a> {
     name: String,
     created: DateTime<Utc>,
     #[serde(serialize_with = "password_hash_serialize")]
+    #[serde(deserialize_with = "password_hash_deserialize", borrow)]
     hash: PasswordHash<'a>,
 }
 
