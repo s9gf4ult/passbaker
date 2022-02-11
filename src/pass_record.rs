e9623ca4188cc9bcb09b5561fed3bbbcaed042ff
Aleksey Uymanov <s9gf4ult@gmail.com>
2022-02-11 18:45:10 +0500

    options

--- src/pass_record.rs
+++ src/pass_record.rs
@@ -1,10 +1,15 @@
 use core::fmt ;
 use pbkdf2::Pbkdf2 ;
-use std::io ;
-use std::path::{Path, PathBuf} ;
-use chrono::prelude::* ;
-use std::todo ;
-use std::fs::* ;
+use std:: {
+    io,
+    path::{Path, PathBuf},
+    default::Default,
+    todo,
+    fs::*,
+} ;
+use chrono::{
+    prelude::*, Duration
+} ;
 use serde:: {
     *, de, de::Visitor
 } ;
@@ -18,6 +23,45 @@ use toml::ser::Error as TomlError ;
 pub trait Interactor {
     fn showMessage(&self, s: &str) ;
     fn readPassword(&self) -> String ;
+}
+
+impl Default for Options {
+    fn default() -> Options {
+        Options {
+            seed: TimingOpts
+            { timeFactor: 2.0,
+              maxInterval: 3600,
+              completion: Some(10)
+            },
+            consolidation: TimingOpts
+            { timeFactor: 2.0 ,
+              maxInterval: 3600*24, // Repeat at least every day
+              completion: Some(10),
+            },
+            retention: TimingOpts
+            { timeFactor: 3.0 ,
+              maxInterval: 3600*24*7, // At least every week
+              completion: None
+            },
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Options {
+    seed: TimingOpts,
+    consolidation: TimingOpts,
+    retention: TimingOpts,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct TimingOpts {
+    // How many times the time intervals must increase after successful attempt
+    timeFactor: f64,
+    // Interval in secs
+    maxInterval: u64,
+    // How mucs successful attempts we need to complete the stage
+    completion: Option<u64>,
 }
 
 pub struct PassRecord<'a> {
