f4caeb8034c4ab9a64a7c1525ebe0588a0b9050d
Aleksey Uymanov <s9gf4ult@gmail.com>
2022-02-23 01:41:46 +0500

    fmt

--- src/attempts.rs
+++ src/attempts.rs
@@ -1,20 +1,11 @@
-use serde::{Serialize, Deserialize} ;
-use chrono::{
-    prelude::*, Duration,
-} ;
-use std:: {
-    path::{PathBuf},
-    fs::*,
-} ;
-use csv::{WriterBuilder} ;
-use crate::{
-    err::*,
-    aux::*,
-    options::*,
-} ;
+use chrono::{prelude::*, Duration};
+use csv::WriterBuilder;
+use serde::{Deserialize, Serialize};
+use std::{fs::*, path::PathBuf};
 
+use crate::{aux::*, err::*, options::*};
 
-pub struct PasswordAttempts (pub Vec<Box<PassAttempt>>) ;
+pub struct PasswordAttempts(pub Vec<Box<PassAttempt>>);
 
 #[derive(Serialize, Deserialize)]
 pub struct PassAttempt {
@@ -25,77 +16,83 @@ pub struct PassAttempt {
 #[derive(Serialize, Deserialize)]
 pub enum AttemptResult {
     Success,
-    Miss
+    Miss,
 }
 
 impl PasswordAttempts {
-    pub fn register_attempt(&mut self, dir: &PathBuf, item: Box<PassAttempt>) -> Result<(), PRError> {
+    pub fn register_attempt(
+        &mut self,
+        dir: &PathBuf,
+        item: Box<PassAttempt>,
+    ) -> Result<(), PRError> {
         let filename = {
-            dir_exists(dir)? ;
-            let date_str = item.timestamp.date().to_string() ;
-            let f = date_str + ".csv" ;
+            dir_exists(dir)?;
+            let date_str = item.timestamp.date().to_string();
+            let f = date_str + ".csv";
             dir.join(f)
-        } ;
+        };
         let file = OpenOptions::new()
             .read(false)
             .create(true)
             .write(true)
             .truncate(false)
             .append(true)
-            .open(&filename)? ;
-        let mut writer = WriterBuilder::new()
-            .has_headers(false).from_writer(&file) ;
-        writer.serialize(&item)? ;
-        writer.flush()? ;
-        self.0.push(item) ;
+            .open(&filename)?;
+        let mut writer = WriterBuilder::new().has_headers(false).from_writer(&file);
+        writer.serialize(&item)?;
+        writer.flush()?;
+        self.0.push(item);
         Ok(())
     }
 
     pub fn next_attempt(
         &self,
         created: &DateTime<Utc>,
-        opts: &Options
+        opts: &Options,
     ) -> Result<(Stage, DateTime<Utc>), PRError> {
-        let mut duration = Duration::seconds(opts.initial.try_into()?) ;
-        let mut res = *created + duration ;
-        let mut stage = Stage::Seed ;
-        let mut successful: u64 = 0 ; // Successful attempts count
-        let seed_complete = opts.seed.completion ;
+        let mut duration = Duration::seconds(opts.initial.try_into()?);
+        let mut res = *created + duration;
+        let mut stage = Stage::Seed;
+        let mut successful: u64 = 0; // Successful attempts count
+        let seed_complete = opts.seed.completion;
         let cons_complete = seed_complete.and_then(|seed| {
-            opts.consolidation.completion.and_then(|cons| { Some(seed + cons) })
-        }) ;
+            opts.consolidation
+                .completion
+                .and_then(|cons| Some(seed + cons))
+        });
 
         for attempt in &self.0 {
             if let AttemptResult::Success = &attempt.result {
-                successful += 1 ;
+                successful += 1;
             };
             match &stage {
                 Stage::Seed => match seed_complete {
                     Some(complete) if successful >= complete => {
-                        stage = Stage::Consolidate ;
-                    },
+                        stage = Stage::Consolidate;
+                    }
                     _ => (),
                 },
                 Stage::Consolidate => match cons_complete {
                     Some(complete) if successful >= complete => {
-                        stage = Stage::Retent ;
-                    },
+                        stage = Stage::Retent;
+                    }
                     _ => (),
                 },
                 _ => (),
-            } ;
-            let timings = opts.stage_timings(&stage) ;
+            };
+            let timings = opts.stage_timings(&stage);
             let factor = match &attempt.result {
                 AttemptResult::Success => timings.succ_factor,
-                AttemptResult::Miss =>    timings.miss_factor,
-            } ;
-            let prev_secs = duration.num_seconds() as f64 ;
-            let new_secs: i64 = timings.max_interval.min(
-                (factor * prev_secs).round() as u64
-            ).try_into()? ;
-            duration = Duration::seconds(new_secs) ;
-            res = attempt.timestamp + duration ;
-        };
+                AttemptResult::Miss => timings.miss_factor,
+            };
+            let prev_secs = duration.num_seconds() as f64;
+            let new_secs: i64 = timings
+                .max_interval
+                .min((factor * prev_secs).round() as u64)
+                .try_into()?;
+            duration = Duration::seconds(new_secs);
+            res = attempt.timestamp + duration;
+        }
         Ok((stage, res))
     }
 }
--- src/pass_record.rs
+++ src/pass_record.rs
@@ -1,29 +1,20 @@
-use pbkdf2::Pbkdf2 ;
-use std:: {
+use chrono::prelude::*;
+use password_hash::{PasswordHasher, PasswordVerifier, SaltString};
+use pbkdf2::Pbkdf2;
+use std::{
+    default::Default,
+    fs::*,
     io,
     path::{Path, PathBuf},
     thread::sleep,
-    default::Default,
-    fs::*,
     time::Duration,
-} ;
-use chrono::{
-    prelude::*
-} ;
-use password_hash:: {
-    SaltString, PasswordHasher, PasswordVerifier,
-} ;
-use crate::{
-    err::*,
-    header::*,
-    attempts::*,
-    options::*,
 };
 
+use crate::{attempts::*, err::*, header::*, options::*};
 
 pub trait Interactor {
-    fn read_password(&mut self) -> String ;
-    fn show_message(&mut self, s: &str) ;
+    fn read_password(&mut self) -> String;
+    fn show_message(&mut self, s: &str);
     fn pay_attention(&mut self, s: &str) {
         self.show_message(s);
     }
@@ -34,67 +25,74 @@ pub struct PassRecord<'a> {
     attempts: PasswordAttempts,
 }
 
-impl <'a> PassRecord<'a> {
+impl<'a> PassRecord<'a> {
     /// Initiates the new record by asking the password twice and creating all
     /// files for further operation.
-    pub fn init<'b> ( dir: &PathBuf,
-                      salt: &'a SaltString,
-                      inter: &mut impl Interactor,
-                      name: String
-    ) -> Result<PassRecord<'a> ,PRError> {
+    pub fn init<'b>(
+        dir: &PathBuf,
+        salt: &'a SaltString,
+        inter: &mut impl Interactor,
+        name: String,
+    ) -> Result<PassRecord<'a>, PRError> {
         let hash = {
-            let pass = inter.read_password() ;
+            let pass = inter.read_password();
             Pbkdf2.hash_password(pass.as_bytes(), salt)?
-        } ;
-        inter.show_message("Repeat the password") ;
-        let pass2 = inter.read_password() ;
-        Pbkdf2.verify_password(pass2.as_bytes(), &hash)? ;
+        };
+        inter.show_message("Repeat the password");
+        let pass2 = inter.read_password();
+        Pbkdf2.verify_password(pass2.as_bytes(), &hash)?;
 
-        PassRecord::check_work_dir(dir)? ;
+        PassRecord::check_work_dir(dir)?;
         let header = PassHeader {
             created: Utc::now(),
             hash: hash,
             name: name,
-            options: Default::default()
-        } ;
+            options: Default::default(),
+        };
 
-        header.create_configs(dir)? ;
+        header.create_configs(dir)?;
 
         let result = PassRecord {
             header: header,
             attempts: PasswordAttempts(vec![]),
-        } ;
+        };
         Ok(result)
     }
 
-    pub fn seed_cycle(&mut self, home: &PathBuf, inter: &mut impl Interactor) -> Result<(), PRError> {
-        while let (Stage::Seed, next) =
-            self.attempts.next_attempt(&self.header.created, &self.header.options)? {
-                let mut now = Utc::now() ;
-                while next > now {
-                    sleep(Duration::from_secs(1));
-                    now = Utc::now() ;
-                    let msg = format!("There is {} seconds left", (next - now).num_seconds()) ;
-                    inter.show_message(&msg);
-                } ;
-                let dir = self.header.attempts_dir_name(home);
-                inter.pay_attention("Enter password:") ;
-                let mut s = inter.read_password() ;
-                while ! self.header.check_pass(&s)? {
-                    let attempt = Box::new(PassAttempt{
-                        timestamp: Utc::now(),
-                        result: AttemptResult::Miss,
-                    });
-                    self.attempts.register_attempt(&dir, attempt)? ;
-                    inter.show_message("Wrong password, try again") ;
-                    s = inter.read_password() ;
-                } ;
-                let attempt = Box::new(PassAttempt{
+    pub fn seed_cycle(
+        &mut self,
+        home: &PathBuf,
+        inter: &mut impl Interactor,
+    ) -> Result<(), PRError> {
+        while let (Stage::Seed, next) = self
+            .attempts
+            .next_attempt(&self.header.created, &self.header.options)?
+        {
+            let mut now = Utc::now();
+            while next > now {
+                sleep(Duration::from_secs(1));
+                now = Utc::now();
+                let msg = format!("There is {} seconds left", (next - now).num_seconds());
+                inter.show_message(&msg);
+            }
+            let dir = self.header.attempts_dir_name(home);
+            inter.pay_attention("Enter password:");
+            let mut s = inter.read_password();
+            while !self.header.check_pass(&s)? {
+                let attempt = Box::new(PassAttempt {
                     timestamp: Utc::now(),
-                    result: AttemptResult::Success,
+                    result: AttemptResult::Miss,
                 });
-                self.attempts.register_attempt(&dir, attempt)? ;
-            };
+                self.attempts.register_attempt(&dir, attempt)?;
+                inter.show_message("Wrong password, try again");
+                s = inter.read_password();
+            }
+            let attempt = Box::new(PassAttempt {
+                timestamp: Utc::now(),
+                result: AttemptResult::Success,
+            });
+            self.attempts.register_attempt(&dir, attempt)?;
+        }
         Ok(())
     }
 
@@ -104,15 +102,17 @@ impl <'a> PassRecord<'a> {
                 io::ErrorKind::NotFound => {
                     // Ok just create the directory
                     create_dir(dir)?;
-                },
-                _ => return Err(PRError::from(e))
+                }
+                _ => return Err(PRError::from(e)),
             },
             Ok(meta) => {
-                if  !meta.file_type().is_dir() {
-                    return Err(PRError::HomeDirectoryError("File name exists but this is not a directory".to_string()))
+                if !meta.file_type().is_dir() {
+                    return Err(PRError::HomeDirectoryError(
+                        "File name exists but this is not a directory".to_string(),
+                    ));
                 }
             }
-        } ;
+        };
         Ok(())
     }
 }
